6f205b188f20a25385f8775fa8c495566379b813
danielromeroc <77584642+danielromeroc@users.noreply.github.com>
2023-02-15 00:11:18 +0000

    Error control if no previous last logon time

--- iRule.tcl
+++ iRule.tcl
@@ -1,15 +1,16 @@
 when ACCESS_POLICY_AGENT_EVENT {
   if { [ACCESS::policy agent_id] eq "lastLogonTime" } {
-    set last_login_time [clock format [clock seconds] -format "%Y-%m-%d-%H%M%S"]
+    set last_login_time [clock format [clock seconds] -format "%Y-%m-%d %H:%M"]
     set username [ACCESS::session data get session.logon.last.username]
     set previous_login_time [table lookup -subtable llt $username]
 
     # Store the previous login time in an APM session variable
-    ACCESS::session data set session.custom.previousLastLogon $previous_login_time
-
-    # NO NEED TO (Commented) Remove the previous login time from the table
-    # table delete -subtable llt $username
-
+    if { $previous_login_time ne ""} then {
+        ACCESS::session data set session.custom.previousLastLogon $previous_login_time
+    } else {
+        ACCESS::session data set session.custom.previousLastLogon "Desconocido"
+    }
+    
     # Store the last login time in the table
     table set -subtable llt $username $last_login_time "indef" "indef"
   }
